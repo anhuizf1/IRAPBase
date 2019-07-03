72117b540a81758bf6fdaa841b0203661e465262
张峰 <feng.zhang@chinairap.com>
2019-07-03 12:17:21 +0800

    王鹏增加实体节点属性 SystemNodeEntity

---
+++ yunhua/Entities/SystemNodeEntity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRAPBase.Entities
+{
+    [Table("stb012")]
+    public  class SystemNodeEntity : BaseEntity
+    {
+        public long PartitioningKey { get; set; }
+        public int NodeID { get; set; }
+        public string HotKey { get; set; }
+        public string AccelerateKey { get; set; }
+        public int MicroHelpNameID { get; set; }
+        public int ToolBarItemHintNameID { get; set; }
+        public int T291LeafID_ToolBarItemIcon { get; set; }
+        public bool NewMenuGroup { get; set; }
+        public bool ToolBarNewSpace { get; set; }
+    }
+
+    public class SystemNodeEntityMap : EntityTypeConfiguration<SystemNodeEntity>
+    {
+        public SystemNodeEntityMap()
+        {
+            //表定义
+            HasKey(t => new { t.PartitioningKey, t.NodeID });
+            //   Property(p => p.LanguageID).HasColumnType("smallint");
+        }
+    }
+}
